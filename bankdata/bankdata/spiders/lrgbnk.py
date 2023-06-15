03288c724dfe0c7d51e389201aff8bad52380f31
Todd Schanzlin <toddschanzlin@yahoo.com>
2023-06-14 22:38:41 -0400

    Updated with write to csv; still need to fix local parsing

--- bankdata/bankdata/spiders/lrgbnk.py
+++ bankdata/bankdata/spiders/lrgbnk.py
@@ -1,4 +1,5 @@
 import scrapy
+import csv
 
 
 class LrgbnkSpider(scrapy.Spider):
@@ -9,16 +10,32 @@ class LrgbnkSpider(scrapy.Spider):
 
     def parse(self, response):
         for child in response.xpath('//table'):
-            if len(child.xpath('tr')) > 200:
+            if len(child.xpath('tr')) > 100:
                 table = child
                 break
+
         count = 0
+
+        csv_file = open('banks.csv', 'w')
+        writer = csv.writer(csv_file)
+        writer.writerow(['bank', 'rank', 'hq city', 'tot assets',
+                        'dom assets', 'dom branches', 'tot branches'])
+
         for row in table.xpath('//tr'):
+            if count > 200:
+                break
             try:
-                print(row.xpath('td//text()').extract())
+                bank = row.xpath('td//text()')[0].extract()
+                rank = row.xpath('td//text()')[1].extract()
+                hq = row.xpath('td//text()')[3].extract()
+                tot_assets = row.xpath('td//text()')[5].extract()
+                dom_assets = row.xpath('td//text()')[6].extract()
+                dom_branches = row.xpath('td//text()')[9].extract()
+                tot_branches = row.xpath('td//text()')[10].extract()
+                writer.writerow([bank, rank, hq, tot_assets,
+                                dom_assets, dom_branches, tot_branches])
                 print(count)
                 count += 1
-                if count > 20:
-                    break
             except IndexError:
                 pass
+        csv_file.close()
